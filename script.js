f4c906e9a4709a1729571ec47a60103fe7e5205c
Iamatemp <103115489+Iamatemp@users.noreply.github.com>
2022-04-06 21:23:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// var x = new Vue({
+//     el: "#x",
+//     data: {
+//         press1: false,
+//         press2: false,
+//         press3: false,
+//         press4: false,
+//     },
+//     methods: {
+//       card1: function() {
+//         this.press1 = !this.press1;
+//       },
+//       card2: function() {
+//         this.press2 = !this.press2;
+//       },
+//       card3: function() {
+//         this.press3 = !this.press3;
+//       },
+//       card4: function() {
+//         this.press4 = !this.press4;
+//       }
+
+//     }
+//   });
+
+function h1(){
+    document.getElementById("1").style.display = "flex";
+}
+function h2(){
+  document.getElementById("2").style.display = "flex";
+}
+function h3(){
+  document.getElementById("3").style.display = "flex";
+}
+function h4(){
+  document.getElementById("4").style.display = "flex";
+}
+function close1(){
+  document.getElementById("1").style.display = "none";
+}
+function close2(){
+  document.getElementById("2").style.display = "none";
+}
+function close3(){
+  document.getElementById("3").style.display = "none";
+}
+function close4(){
+  document.getElementById("4").style.display = "none";
+}
